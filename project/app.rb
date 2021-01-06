451c6cf40758b27f72bd2eae23f75cb52b068c3d
rashidat <rasidatb@gmail.com>
2021-01-06 15:56:08 +0000

    add image with a border

--- project/app.rb
+++ project/app.rb
@@ -8,4 +8,14 @@ get '/' do
     'Your an alien'
   end
 
- 
+#    Rack::Static
+
+  post '/' do
+ 'cute dog pic'
+  end
+
+  get '/puppy' do
+ "<div style='border: 2px dashed red'>
+   <img src='https://www.dogtime.com/assets/uploads/2011/03/puppy-development-1280x720.jpg'>
+ </div>"
+  end
